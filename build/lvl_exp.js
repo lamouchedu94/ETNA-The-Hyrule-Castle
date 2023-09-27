76fb36ab308909682d72a350f266862b39d1389f
Shayman <s.s.1604manganise@gmail.com>
2023-09-27 16:40:45 +0200

    [DEL] Revmove unwanted files

--- build/lvl_exp.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const minXp = 15;
-const maxXp = 50;
-function gainXp(hero) {
-    const xp = Math.floor(Math.random() * (maxXp - minXp + 1) + minXp);
-    hero.addXp(xp);
-}
-exports.default = gainXp;
