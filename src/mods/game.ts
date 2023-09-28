0b7846d0785542b173c4182ccad611c2d6cb0737
cassan_p <cassan_p@etna-alternance.net>
2023-09-28 18:07:49 +0200

    [ADD] Almost nothing

--- src/mods/game.ts
+++ src/mods/game.ts
@@ -78,5 +78,7 @@ export function menu(userInput : number) {
   if (userInput === 5) {
     console.log(`\nSee you later !`)     
     process.exit(1)
+  } else {
+    console.log("not implemented yep. wip")
   }
 }
