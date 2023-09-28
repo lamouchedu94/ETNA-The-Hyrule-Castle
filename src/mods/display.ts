7007fcbf33c1ed98811d731b4e13e0644aaf840d
cassan_p <cassan_p@etna-alternance.net>
2023-09-28 16:34:53 +0200

    [ADD] Function display menu

--- src/mods/display.ts
+++ src/mods/display.ts
@@ -1,10 +1,24 @@
 import Hero from './Hero';
 import Enemy from './Enemy';
+import getUserInput from './userInput';
 
-export default function displayRound(floor : number, hero : Hero, ennemy : Enemy) {
+export function displayRound(floor : number, hero : Hero, ennemy : Enemy) {
   console.log(`================== FIGHT ${floor} ==================`);
   ennemy.displayInfo();
   hero.displayInfo();
   console.log('—————————————————— OPTION ——————————————————');
-  console.log('1 - Attack \t 2 - Heal \t 3 - View Inventory \t 4 - Quit');
+  console.log('1 - Attack \t 2 - Heal \t 3 - View Inventory \t 4 - Leave fight \t 5 - Game Menu');
+}
+
+export function displayMenu() {
+  console.log(`================== GAME MENU ==================`)
+  console.log(`1 Use objects`)
+  console.log(`2 Save`)
+  console.log(`3 Save and Quit`)
+  console.log(`4 Quit (witout saving)`)
+  const userInput = getUserInput()
+  if (userInput === 4) {
+    console.log(`\nSee you later !`)
+    process.exit(1)
+  }
 }
